e8593636e9b0f7664c20e8f6f04ea2c953716792
szydob <117864567+szydob@users.noreply.github.com>
2023-05-04 18:21:10 +0200

    Update okna gracza
    dodanie kodu sprawdzającego interakcje gracza z programem

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ dis=pygame.display.set_mode((400,300))
 pygame.display.update()
 pygame.display.set_caption('Snake game')
 game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)   #prints out all the actions that take place on the screen
+ 
 pygame.quit()
 quit()
