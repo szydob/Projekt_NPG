b3ec24115b0825d24b4d80cef1d57ea5bdb0add3
szydob <117864567+szydob@users.noreply.github.com>
2023-05-05 14:21:35 +0200

    update
    dodano linie 7 i 8, usunięto 4

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 pygame.init()
 dis=pygame.display.set_mode((400,300))
-pygame.display.update()
+
 pygame.display.set_caption('Snake game')
+
+blue=(0,0,255)
+red=(255,0,0)
+
 game_over=False
 while not game_over:
     for event in pygame.event.get():
