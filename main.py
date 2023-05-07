c8d5e3dd89f919a1764661cb1e2ac5f1db8e4764
Geq0nek <tomaszdrag19@gmail.com>
2023-05-07 19:10:07 +0200

    Interakcja gracza
    Sprawdzanie czy gracz kliknął klawisz odpowiadający za ruch w dół

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((800,600))
+dis = pygame.display.set_mode((800, 600))
 
 pygame.display.set_caption('Snake game')
 
 white = (255, 255, 255)
 black = (0, 0, 0)
-red=(255,0,0)
+red = (255, 0, 0)
 
-game_over=False
+game_over = False
 
 x1 = 300
 y1 = 300
@@ -21,14 +21,17 @@ clock = pygame.time.Clock()
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_over=True
+            game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 x1_change = -10
                 y1_change = 0
             elif event.key == pygame.K_RIGHT:
                 x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
                 
+
     pygame.draw.rect(dis, black, [x1, y1, 10, 10])
 
     clock.tick(30)
