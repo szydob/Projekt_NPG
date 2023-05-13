3e26041f9961bfaef96ed7d2f1b524163b3977b3
Geq0nek <tomaszdrag19@gmail.com>
2023-05-13 18:18:58 +0200

    Zmienne
    zmienne gry odpowiedzialne za kolor i czcionkę

--- variables.py
+++ variables.py
@@ -11,3 +11,17 @@ dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption("Wonsz z Duszą")
 icon = pygame.image.load("assets/hero.png")
 pygame.display.set_icon(icon)
+
+clock = pygame.time.Clock()
+
+#font and styels variables
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+
+#color variables
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
