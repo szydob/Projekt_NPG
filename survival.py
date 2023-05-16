131f372a3c119804fe9d93cfa4a98af24d11a0ba
midusza <129391615+midusza@users.noreply.github.com>
2023-05-16 19:46:23 +0200

    Update survival.py
    Dodanie stanów gry

--- survival.py
+++ survival.py
@@ -30,3 +30,15 @@ def survival():
         while game_close == True:
             dis.fill(blue)
             pygame.display.update()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        survival()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
