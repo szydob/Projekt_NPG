9d23d1eb016bc9a3d163d0fbc587487b38844eac
midusza <129391615+midusza@users.noreply.github.com>
2023-05-23 19:34:03 +0200

    Koniec

--- survival.py
+++ survival.py
@@ -99,3 +99,41 @@ def survival():
 
         snake_List_1.append(snake_Head_1)
         snake_List_2.append(snake_Head_2)
+        
+        if len(snake_List_1) > Length_of_snake_1:
+            del snake_List_1[0]
+
+        if len(snake_List_2) > Length_of_snake_2:
+             del snake_List_2[0]
+
+        for x in snake_List_1[:-1]:
+            if x == snake_Head_1:
+                game_close = True
+
+        for x in snake_List_2[:-1]:
+            if x == snake_Head_2:
+                game_close = True
+
+        our_snake(snake_block, snake_List_1, black)
+        our_snake(snake_block, snake_List_2, red)
+
+        Your_score(Length_of_snake_1 - 1, 1)
+        Your_score(Length_of_snake_2 - 1, 2)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake_1 += 1
+
+        if x2 == foodx and y2 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake_2 += 1
+            
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
