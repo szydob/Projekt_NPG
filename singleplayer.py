e8a17c30044fb8305d973d41a4ca995d7b51e40c
Geq0nek <tomaszdrag19@gmail.com>
2023-05-24 12:35:04 +0200

    Powiększanie węża
    Kod powiększający węża gdy zjadł owoc

--- singleplayer.py
+++ singleplayer.py
@@ -61,3 +61,22 @@ def single_player():
         snake_List.append(snake_Head)
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List, black)
+        Your_score(Length_of_snake - 1, 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
